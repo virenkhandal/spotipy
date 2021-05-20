49148a528ff523c5cdb8995795765ced26ad9f17
Viren Khandal <virenkhandal@berkeley.edu>
2021-05-20 16:50:58 -0700

    updating to login page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from flask.templating import render_template_string
 from spotify import *
 import jinja2
 env = jinja2.Environment()
@@ -9,12 +10,16 @@ from flask import request
 # matplotlib.use('Agg')
 app = Flask(__name__)
 
-auth_payload = {'client_id': '61bb4c3ea3c24253a738bd8f34956191', 'response_type': 'token', 'redirect_uri': 'https://spotipy1.herokuapp.com/results'}
+auth_payload = {'client_id': '61bb4c3ea3c24253a738bd8f34956191', 'response_type': 'token', 'redirect_uri': 'http://spotipy1.herokuapp.com/results'}
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
-    # requests.get('https://accounts.spotify.com/authorize', params=auth_payload)
-    return render_template('index.html')
+    r = requests.get('https://accounts.spotify.com/authorize', params=auth_payload)
+    token = request.args.get('access_token')
+    # print(r.text)
+    # artists = getArtists(token)
+    # tracks = getTracks(token)
+    return render_template_string(r.text)
     
 
 @app.route('/results', methods=['GET', 'POST'])
