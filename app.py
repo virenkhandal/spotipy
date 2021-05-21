74dfe17d787162a1aeff097b2198ccef148b6bc4
Viren Khandal <virenkhandal@berkeley.edu>
2021-05-20 19:19:02 -0700

    fixing routing cuz this doesnt allow me to obtain the anchor parameter

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def homepage():
     requests.get('https://accounts.spotify.com/authorize?client_id=61bb4c3ea3c24253a738bd8f34956191&response_type=token&redirect_uri=https%3A%2F%2Fspotipy1.herokuapp.com%2Fresults')
     return render_template('index.html')
 
-@app.route('/results', methods=['GET', 'POST'])
+@app.route('/results#', methods=['GET', 'POST'])
 def results(token):
     # url = URL()
     token = request.args.get('token')
