9f51edd8185bf44190f790dd1a2508a8715560e3
Viren Khandal <virenkhandal@berkeley.edu>
2021-05-20 18:43:24 -0700

    updating cleaner flow to login

--- app.py
+++ app.py
@@ -16,11 +16,6 @@ auth_payload = {'client_id': '61bb4c3ea3c24253a738bd8f34956191', 'response_type'
 def homepage():
     requests.get('https://accounts.spotify.com/authorize?client_id=61bb4c3ea3c24253a738bd8f34956191&response_type=token&redirect_uri=https%3A%2F%2Fspotipy1.herokuapp.com%2Fresults/')
     return render_template('index.html')
-    
-@app.route('/en/login', methods=['GET', 'POST'])
-def login():
-    r = requests.get('https://accounts.spotify.com/authorize?client_id=61bb4c3ea3c24253a738bd8f34956191&response_type=token&redirect_uri=https%3A%2F%2Fspotipy1.herokuapp.com%2Fresults/')
-    return render_template_string(r.text)
 
 @app.route('/results/', methods=['GET', 'POST'])
 def results():
