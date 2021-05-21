6ac5c48dbd4287324075a4834a1177920edebed5
Viren Khandal <virenkhandal@berkeley.edu>
2021-05-20 23:30:33 -0700

    added full authorization flow

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask.templating import render_template_string
+from werkzeug.utils import redirect
 from spotify import *
 import jinja2
 env = jinja2.Environment()
@@ -10,21 +11,34 @@ from urlpath import URL
 # import matplotlib
 # matplotlib.use('Agg')
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 auth_payload = {'client_id': '61bb4c3ea3c24253a738bd8f34956191', 'response_type': 'token', 'redirect_uri': 'https%3A%2F%2Fspotipy1.herokuapp.com%2Fresults'}
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
-    requests.get('https://accounts.spotify.com/authorize?client_id=61bb4c3ea3c24253a738bd8f34956191&response_type=token&redirect_uri=https%3A%2F%2Fspotipy1.herokuapp.com%2Fresults')
-    return render_template('index.html')
+    # auth_url = 'https://accounts.spotify.com/authorize?client_id=61bb4c3ea3c24253a738bd8f34956191&response_type=token&redirect_uri=https%3A%2F%2Fspotipy1.herokuapp.com%2Fresults'
+    auth_url = 'http://accounts.spotify.com/authorize?client_id=61bb4c3ea3c24253a738bd8f34956191&response_type=token&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2Fresults'
+    return redirect(auth_url)
 
-@app.route('/results#', methods=['GET', 'POST'])
-def results(token):
-    # url = URL()
-    token = request.args.get('token')
-    print(token)
-    artists = getArtists(token)
-    tracks = getTracks(token)
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    session.clear()
+    code = request.args.get('code')
+    print("code: ", code)
+    auth_token_url = f"https://accounts.spotify.com/api/token"
+    res = requests.post(auth_token_url, data={
+        "grant_type":"authorization_code",
+        "code":code,
+        "redirect_uri":"http://127.0.0.1:5000/results",
+        "client_id":'61bb4c3ea3c24253a738bd8f34956191',
+        "client_secret":'43e1501fc8d94c768d8af79f096395eb'
+        })
+    res_body = res.json()
+    print(res.json())
+    session["toke"] = res_body.get("access_token")
+    print("token: ", session['toke'])
+    artists = getArtists(session["toke"])
+    tracks = getTracks(session["toke"])
     return render_template('results.html', artists=artists, tracks=tracks)
 
 if __name__ == '__main__':
